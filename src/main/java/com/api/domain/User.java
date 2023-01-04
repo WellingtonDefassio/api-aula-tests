3f496dc646efe012ba63ac485d682fe0ac9fdeba
Wellington Defassio <well.defassio@gmail.com>
2023-01-03 21:49:57 -0300

    chore: classe usuario

---
+++ src/main/java/com/api/domain/User.java
@@ -0,0 +1,22 @@
+package com.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+}
